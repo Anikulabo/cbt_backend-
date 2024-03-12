4f083791d7b00e3826b132444d7842391ef43517
unknown <peterskelvin123@gmail.com>
2024-03-12 00:50:58 -0700

    still grinding

--- models/users.js
+++ models/users.js
@@ -1,31 +1,44 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database.js');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database.js");
 
-const User = sequelize.define('User', {
-  // Define the columns of the 'Users' table
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true
+const User = sequelize.define(
+  "User",
+  {
+    // Define the columns of the 'Users' table
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true,
+    }
+    ,score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }, 
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false
+  {
+    // Other model options
+    tableName: "Users", // Name of the table in the database
+    timestamps: true, // Include timestamps (createdAt, updatedAt)
   }
-}, {
-  // Other model options
-  tableName: 'Users', // Name of the table in the database
-  timestamps: true // Include timestamps (createdAt, updatedAt)
-});
+);
 
 module.exports = User;
